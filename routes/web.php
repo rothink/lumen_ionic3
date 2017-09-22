4a2945a97e1574f3e8073e195b30f99abc7583ad
rothink <rodrigoluz@live.com>
2017-09-21 23:10:02 -0300

    crud completo

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ $router->get('/', function () use ($router) {
 
 $router->group(['prefix' => 'api/v1', 'namespace' => 'Api\V1'], function() use ($router) {
     $router->get('restaurants', 'RestaurantsController@index');
+    $router->get('restaurants/{id}', 'RestaurantsController@show');
+    $router->post('restaurants', 'RestaurantsController@store');
+    $router->put('restaurants/{id}', 'RestaurantsController@update');
+    $router->delete('restaurants/{id}', 'RestaurantsController@destroy');
 });
