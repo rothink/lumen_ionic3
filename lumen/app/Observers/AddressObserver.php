351300bfb00d39057d06b68ed140763fe4f4cd4d
rothink <rodrigoluz@live.com>
2018-01-15 22:40:04 -0200

    comecando geolocation

--- lumen/app/Observers/AddressObserver.php
+++ lumen/app/Observers/AddressObserver.php
@@ -3,8 +3,7 @@
 namespace App\Observers;
 
 use App\Address;
-use App\Dish;
-
+use AnthonyMartin\GeoLocation\GeoLocation;
 class AddressObserver
 {
     public function creating(Address $model)
@@ -27,6 +26,15 @@ class AddressObserver
             $model->city . '-' .
             $model->state . '-' .
             $model->address;
+
+        $response = GeoLocation::getGeocodeFromGoogle($location);
+
+        if(!empty($response->results) and is_array($response->results)) {
+            $result = array_pop($response->results);
+            $model->latitude = $result->geometry->location->lat;
+            $model->longitude = $result->geometry->location->lng;
+
+        }
     }
 
 }
