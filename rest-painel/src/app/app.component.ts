48d6d1bc4a68a5c27194a6f3b58d902c1768029a
rothink <rodrigoluz@live.com>
2018-01-27 15:51:48 -0200

    restt painel event emit

--- rest-painel/src/app/app.component.ts
+++ rest-painel/src/app/app.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import * as jQuery from 'jquery';
+import {AuthService} from './user/services/auth.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+    me = null;
 
-  ngOnInit() {
-    jQuery("#showMenu").sideNav();
-  }
+
+    constructor(private authService: AuthService) {
+    }
+
+    ngOnInit() {
+        jQuery("#showMenu").sideNav();
+
+        this.authService
+            .eventEmitter
+            .subscribe(() => {
+                this.getUserData();
+            });
+    }
+
+    protected getUserData() {
+        this.authService.getUser()
+            .then((res) => {
+                this.me = res;
+                if(res.status === 401) {
+                    this.me = null;
+                }
+            })
+            .catch((error) => {
+                this.me = null;
+            });
+    }
 }
